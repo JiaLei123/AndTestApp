cc962566a9dc8fa9079e2b4834bb1b9b5d5c076f
jialei <sfw123817@gmail.com>
2016-04-13 23:26:53 +0800

    Initial commit

--- app/src/main/java/com/example/jiayu/helloworld/MainActivity.java
+++ app/src/main/java/com/example/jiayu/helloworld/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.jiayu.helloworld.Uiltity.TestUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TestUtil ut = new TestUtil();
+
+        TextView tv = (TextView) findViewById(R.id.MyUtityView);
+
+        tv.setText(ut.GetValue());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
