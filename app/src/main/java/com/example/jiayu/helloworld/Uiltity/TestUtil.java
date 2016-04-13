ecef54cdce478e6d9ac84929e74dc91e8948a863
yun jia <yun jia>
2016-04-13 22:51:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jiayu/helloworld/Uiltity/TestUtil.java
@@ -0,0 +1,10 @@
+package com.example.jiayu.helloworld.Uiltity;
+
+/**
+ * Created by jiayu on 2016/4/11.
+ */
+public class TestUtil {
+    public String GetValue() {
+        return "Hellow From Utltiy Class";
+    }
+}
