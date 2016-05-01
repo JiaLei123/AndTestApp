66da685509febb90f1c7afa6e8057f64de99d47d
jialei <sfw123817@gmail.com>
2016-05-01 23:42:09 +0800

    Initial commit

--- app/src/main/java/com/example/jiayu/helloworld/MainActivity.java
+++ app/src/main/java/com/example/jiayu/helloworld/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -42,9 +41,14 @@ public class MainActivity extends AppCompatActivity {
     private void showMyText() {
         TestUtil myut = new TestUtil();
 
-        TextView tv = (TextView) findViewById(R.id.MyUtityView);
+        TextView tv = (TextView) findViewById(R.id.Show);
 
         tv.setText(myut.GetValue());
+    }
+
+    public void clickHandler(View source) {
+        TextView tv = (TextView) findViewById(R.id.Show);
+        tv.setText("Hellow Andriod - " + new java.util.Date());
     }
 
     @Override
