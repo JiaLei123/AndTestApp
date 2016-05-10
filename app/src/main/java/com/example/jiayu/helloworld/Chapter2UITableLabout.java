26a72f40b5c58c73e61348413f6cc9e21ff389ab
jialei <sfw123817@gmail.com>
2016-05-11 00:05:24 +0800

    Initial commit

--- app/src/main/java/com/example/jiayu/helloworld/Chapter2UITableLabout.java
+++ app/src/main/java/com/example/jiayu/helloworld/Chapter2UITableLabout.java
@@ -3,12 +3,11 @@ package com.example.jiayu.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Chapter2UICustomView extends AppCompatActivity {
+public class Chapter2UITableLabout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chapter2_uicustom_view);
+        setContentView(R.layout.activity_chapter2_uitable_labout);
     }
-
 }
