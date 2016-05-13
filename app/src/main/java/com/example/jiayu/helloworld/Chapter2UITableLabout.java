864b598e4e7c4a0c0bbf7321ec3b497c3395bbad
jialei <sfw123817@gmail.com>
2016-05-13 21:15:02 +0800

    Initial commit

--- app/src/main/java/com/example/jiayu/helloworld/Chapter2UITableLabout.java
+++ app/src/main/java/com/example/jiayu/helloworld/Chapter2UITableLabout.java
@@ -9,5 +9,6 @@ public class Chapter2UITableLabout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter2_uitable_labout);
+
     }
 }
