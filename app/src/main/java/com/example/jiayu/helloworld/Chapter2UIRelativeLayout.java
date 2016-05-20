ffa3b6d3a6b2fdfb747a37c75d39f3f1c958abc1
JiaLei <sfw123817@gmail.com>
2016-05-20 08:06:05 +0800

    relativeLayout

---
+++ app/src/main/java/com/example/jiayu/helloworld/Chapter2UIRelativeLayout.java
@@ -0,0 +1,13 @@
+package com.example.jiayu.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chapter2UIRelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter2_uirelative_layout);
+    }
+}
