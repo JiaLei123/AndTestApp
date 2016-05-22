c427e0be6a0d788d9a109300f6dcc5441d4b5961
JiaLei <sfw123817@gmail.com>
2016-05-22 08:36:45 +0800

    GridLayout

--- app/src/main/java/com/example/jiayu/helloworld/Chapter2UIGridLayout.java
+++ app/src/main/java/com/example/jiayu/helloworld/Chapter2UIGridLayout.java
@@ -2,6 +2,8 @@ package com.example.jiayu.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
 import android.widget.GridLayout;
 
 public class Chapter2UIGridLayout extends AppCompatActivity {
@@ -12,8 +14,8 @@ public class Chapter2UIGridLayout extends AppCompatActivity {
             {
               "7", "8", "9", "/",
               "4", "5", "6", "*",
-
-              "1", "0", "=", "+"
+              "1", "2", "3", "-",
+              ".", "0", "=", "+"
             };
 
     @Override
@@ -21,5 +23,18 @@ public class Chapter2UIGridLayout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter2_uigrid_layout);
 
+        gridLayout = (GridLayout)findViewById(R.id.root);
+        for(int i =0;i<chars.length;i++) {
+            Button bn = new Button(this);
+            bn.setText(chars[i]);
+            bn.setTextSize(40);
+            bn.setPadding(5, 35, 5, 35);
+            GridLayout.Spec rowSpec = GridLayout.spec(i / 4 + 2);
+            GridLayout.Spec columnSpec = GridLayout.spec(i % 4);
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec, columnSpec);
+            params.setGravity(Gravity.FILL);
+            gridLayout.addView(bn, params);
+
+        }
     }
 }
