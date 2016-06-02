e5e4d5adda70cd621f944c19f8e0e1382657e4f1
JiaLei <sfw123817@gmail.com>
2016-06-02 08:20:10 +0800

    SimpleListView

---
+++ app/src/main/java/com/example/jiayu/helloworld/Chapter2/Chapter2SimpleListView.java
@@ -0,0 +1,15 @@
+package com.example.jiayu.helloworld.Chapter2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jiayu.helloworld.R;
+
+public class Chapter2SimpleListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter2_simple_list_view);
+    }
+}
