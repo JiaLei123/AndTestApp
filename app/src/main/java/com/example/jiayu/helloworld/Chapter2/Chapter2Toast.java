592d081e88ad276fea796c8668f73e4486cd51cb
JiaLei <sfw123817@gmail.com>
2016-06-26 10:21:25 +0800

    Toast with LayoutInflater

--- app/src/main/java/com/example/jiayu/helloworld/Chapter2/Chapter2Toast.java
+++ app/src/main/java/com/example/jiayu/helloworld/Chapter2/Chapter2Toast.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,6 +22,7 @@ public class Chapter2Toast extends AppCompatActivity {
         setContentView(R.layout.activity_chapter2_toast);
 
         Button simple = (Button) findViewById(R.id.simple);
+        assert simple != null;
         simple.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +33,7 @@ public class Chapter2Toast extends AppCompatActivity {
 
 
         Button bn = (Button) findViewById(R.id.bn);
+        assert bn != null;
         bn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +48,26 @@ public class Chapter2Toast extends AppCompatActivity {
                 textView.setTextSize(24);
                 textView.setTextColor(Color.MAGENTA);
                 li.addView(textView);
+                toast.setView(li);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        Button bn2 = (Button) findViewById(R.id.bn2);
+        assert bn2 != null;
+        bn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = new Toast(Chapter2Toast.this);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                LayoutInflater inflater = LayoutInflater.from(Chapter2Toast.this);
+                View li = inflater.inflate(R.layout.cell_with_text, null);
+                ImageView imageView = (ImageView) li.findViewById(R.id.cellImage);
+                imageView.setImageResource(R.drawable.tools);
+                TextView textView = (TextView) li.findViewById(R.id.cellText);
+                textView.setText("带图片的文字");
+
                 toast.setView(li);
                 toast.setDuration(Toast.LENGTH_LONG);
                 toast.show();
