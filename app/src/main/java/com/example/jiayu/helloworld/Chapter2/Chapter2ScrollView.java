bf418eee5a2172671167932ddf34cbb4e5bbf3a0
JiaLei <sfw123817@gmail.com>
2016-07-13 08:35:52 +0800

    ScrollView

---
+++ app/src/main/java/com/example/jiayu/helloworld/Chapter2/Chapter2ScrollView.java
@@ -0,0 +1,14 @@
+package com.example.jiayu.helloworld.Chapter2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.jiayu.helloworld.R;
+
+public class Chapter2ScrollView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter2_scroll_view);
+    }
+}
